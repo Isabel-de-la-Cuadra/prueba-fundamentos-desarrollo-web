0eb7a546e77c5277fd1d5b306dae661af1d57921
Isabel de la Cuadra <isabel.de.la.cuadra@gmail.com>
2022-03-13 17:08:08 -0300

    Prueba con Correcciones

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,26 +16,30 @@ $(document).ready(function() {
 
         let valueInput = $("#superHeroInput").val();
 
-        $.ajax({
-            type: "GET",
-            url: "https://www.superheroapi.com/api.php/4905856019427443/" + valueInput,
-            dataType: "json",
-            success: function(data) {
-                if (data !== null && data !== undefined) {
+        if (valueInput < 1 || valueInput > 732) {
+            alert(":(  ¡Lo siento! \n El número ingresado está fuera del rango de mi base de datos de SuperHero ")
+        } else {
 
-                    let nombre = data.name;
-                    let conexiones = data.connections["group-affiliation"];
-                    let publicadoPor = data.biography.publisher;
-                    let ocupacion = data.work.occupation;
-                    let primeraAparicion = data.biography["first-appearance"];
-                    let alturaP = data.appearance.height["0"];
-                    let alturaCM = data.appearance.height["1"];
-                    let pesoL = data.appearance.weight["0"];
-                    let pesoK = data.appearance.weight["1"];
-                    let alias = data.biography.aliases;
-                    let imagen = data.image.url
+            $.ajax({
+                type: "GET",
+                url: "https://www.superheroapi.com/api.php/4905856019427443/" + valueInput,
+                dataType: "json",
+                success: function(data) {
+                    if (data !== null && data !== undefined) {
 
-                    $("#superHeroInfo").append(`
+                        let nombre = data.name;
+                        let conexiones = data.connections["group-affiliation"];
+                        let publicadoPor = data.biography.publisher;
+                        let ocupacion = data.work.occupation;
+                        let primeraAparicion = data.biography["first-appearance"];
+                        let alturaP = data.appearance.height["0"];
+                        let alturaCM = data.appearance.height["1"];
+                        let pesoL = data.appearance.weight["0"];
+                        let pesoK = data.appearance.weight["1"];
+                        let alias = data.biography.aliases;
+                        let imagen = data.image.url
+
+                        $("#superHeroInfo").append(`
                         <h4 class="text-start ms-5">SuperHero Encontrado</h4>
                         <br>
                         <div class="card mb-3" style="max-width: 540px;">
@@ -58,59 +62,53 @@ $(document).ready(function() {
                         </div>
                     `)
 
-                    let estadisticas = [];
+                        let estadisticas = [];
 
-                    for (let key in data.powerstats) {
-                        if (data.powerstats !== null && data.powerstats !== undefined) {
+                        for (let key in data.powerstats) {
                             estadisticas.push({ y: data.powerstats[key], name: key });
-                        } else {
-                            $("#chartContainer").append(`
-                            <h4 class="text-center">Lo siento, no tengo información para graficar</h4>`)
                         }
 
-                    }
-
-                    var chart = new CanvasJS.Chart("chartContainer", {
-                        exportEnabled: true,
-                        animationEnabled: true,
-                        title: {
-                            text: `Estadísticas de Poder para ${nombre}`,
-                            fontsize: 5,
-                            fontColor: "blue"
-                        },
-                        legend: {
-                            cursor: "pointer",
-                            itemclick: explodePie,
-                            fontsize: 8,
-                            fontColor: "blue"
-                        },
-                        data: [{
-                            type: "pie",
-                            showInLegend: true,
-                            toolTipContent: "{name}: <strong>{y}</strong>",
-                            indexLabel: "{name} - {y}",
-                            dataPoints: estadisticas,
-                        }]
-                    });
-                    chart.render();
+                        var chart = new CanvasJS.Chart("chartContainer", {
+                            exportEnabled: true,
+                            animationEnabled: true,
+                            title: {
+                                text: `Estadísticas de Poder para ${nombre}`,
+                                fontsize: 5,
+                                fontColor: "blue"
+                            },
+                            legend: {
+                                cursor: "pointer",
+                                itemclick: explodePie,
+                                fontsize: 8,
+                                fontColor: "blue"
+                            },
+                            data: [{
+                                type: "pie",
+                                showInLegend: true,
+                                toolTipContent: "{name}: <strong>{y}</strong>",
+                                indexLabel: "{name} - {y}",
+                                dataPoints: estadisticas,
+                            }]
+                        });
+                        chart.render();
 
 
-                    function explodePie(e) {
-                        if (typeof(e.dataSeries.dataPoints[e.dataPointIndex].exploded) === "undefined" || !e.dataSeries.dataPoints[e.dataPointIndex].exploded) {
-                            e.dataSeries.dataPoints[e.dataPointIndex].exploded = true;
-                        } else {
-                            e.dataSeries.dataPoints[e.dataPointIndex].exploded = false;
+                        function explodePie(e) {
+                            if (typeof(e.dataSeries.dataPoints[e.dataPointIndex].exploded) === "undefined" || !e.dataSeries.dataPoints[e.dataPointIndex].exploded) {
+                                e.dataSeries.dataPoints[e.dataPointIndex].exploded = true;
+                            } else {
+                                e.dataSeries.dataPoints[e.dataPointIndex].exploded = false;
+                            }
+                            e.chart.render();
+
                         }
-                        e.chart.render();
+
 
                     }
-
 
                 }
-
-            }
-        });
-
+            });
+        }
     });
     //fin jquery
 });
